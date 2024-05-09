b89cca2d20bd3174901b5f4a818a2a91763b0096
pk15pi <76875206+pk15pi@users.noreply.github.com>
2024-05-09 16:28:43 +0530

    updated code

--- api/figshare_mapping.py
+++ api/figshare_mapping.py
@@ -23,38 +23,46 @@ from rest_framework.response import Response
 
 # 2. Map Source File into Figshare JSON Format
 # Function to map data to Figshare JSON format
-def map_to_figshare_json(record):
+def map_to_figshare_json(record, is_update):
     
     figshare_json = {
 
-        "title": "USDA Economic Research Service",
-        "authors": ["ECONOMICS > Applied Economics > Agricultural economics"],
-        "categories": ["Dataset"],
+        "title": record['title'],
+        "authors": [{"author":"USDA Economic Research Service"}],
+        # "categories": ["ECONOMICS > Applied Economics > Agricultural economics"],
+        "categories": [1],
+        "item_Type":"Dataset",
         "keyword": record.get("Keyword", ""),
         "description": record.get("description", ""),
-        "posted_date": record.get("Temporal Extent Start Date", ""),
-        "related_material_identifier": record.get("IsSupplementTo", ""),
+        "posted_date": record.get(datetime.now(), None),
+        "funding":"Economic Research Service",
+        "related_material_identifier": record.get("references", ""),
         "relation_type": "IsSupplementTo",
-        "license": "license_choice",
+        # "license": record.get("license",""),
+        "license": 1000,
         "contactPoint": {
-            "fn": record.get("Data Contact Name", ""),
-            "hasEmail": record.get("Data Contact Email", "")
+            "fn": record.get("contactPoint", None).get("fn", None),
+            "hasEmail": record.get("contactPoint", None).get("hasEmail", None)
         },
         "publisher": {
-            "name": record.get("Publisher", "")
+            "name": record.get("publisher", "")
         },
-        "temporal": record.get("temporal", ""),
-        "accrualPeriodicity": record.get("accrualPeriodicity", "Not specified"),
-        "Theme": record.get("spatial", "economy"),
-        "Temporal Extent End Date": record.get("Temporal Extent End Date", ""),
-        "Frequency": record.get("Frequency", ""),
-        "Geographic Coverage": record.get("Geographic Coverage", ""),
-        "ISO Topic Category": ["economics", "agricultural economics"],
-        "NALT terms": record.get("NALT terms", ""),
-        "bureauCode": "005:13",
-        "programCode": "005:13",
-        
+        # "Temporal Extent Start Date": record.get("temporal", ""),
+        # "Temporal_End_Date": datetime.now(),
+        "Frequency": record.get("accrualPeriodicity", ""),
+        "Theme": "Not specifiled",
+        "Geographic_Coverage": record.get("Geographic Coverage", ""),
+        "ISO_Topic_Category":"economy",
+        "NALT_terms": ["economics", "agricultural economics"],
+        "OMB_Bureau_Code": record.get("bureauCode", ""),
+        "OMB_Program_Code": record.get("ProgramCode", ""),
+        "Public_Access_Level":record.get("accessLevel", ""),
     }
+
+    if is_update:
+        figshare_json['posted_date'] = record['issued']
+        figshare_json["Temporal_Start Date"] =  record['issued']
+
     return figshare_json
 
 
@@ -64,18 +72,21 @@ def map_to_figshare_json(record):
 # Function to create articles on Figshare
 def create_figshare_articles(data):
     for record in data:
-        figshare_json = map_to_figshare_json(record)
+        figshare_json = map_to_figshare_json(record, False)
         # Assuming you have Figshare API credentials
         headers = {
             "Content-Type": "application/json",
-            "Authorization": "Bearer YOUR_FIGSHARE_ACCESS_TOKEN"
+            "Authorization": settings.TOKEN
         }
-        response = requests.post('https://api.figshare.com/v2/private_articles', json=figshare_json, headers=headers)
+        try:
+            response = requests.post('https://api.figshare.com/v2/account/articles', json=figshare_json, headers=headers)
+        except Exception as e:
+            print(e)
         if response.status_code == 201:
-            # print(f"Article '{record['title']}' created successfully on Figshare.")
+            print(f"Article '{record['title']}' created successfully on Figshare.")
             return True
         else:
-            # print(f"Failed to create article '{record['title']}' on Figshare. Error: {response.text}")
+            print(f"Failed to create article '{record['title']}' on Figshare.", response.status_code, response.content)
             return False
 
 
@@ -86,12 +97,13 @@ def create_figshare_articles(data):
 # Function to check if an article exists on Figshare
 def check_if_article_exists(title):
     headers = {
-        "Authorization": "Bearer YOUR_FIGSHARE_ACCESS_TOKEN"
+        "Content-Type": "application/json",
+        "Authorization": settings.TOKEN
     }
     params = {
         "title": title
     }
-    response = requests.get('https://api.figshare.com/v2/private_articles', params=params, headers=headers)
+    response = requests.get('https://api.figshare.com/v2/private_articles/', params=params, headers=headers)
     if response.status_code == 200:
         articles = response.json()
         if articles:
@@ -102,12 +114,12 @@ def check_if_article_exists(title):
 
 # Function to update an existing article on Figshare
 def update_figshare_article(article_id, data):
-    figshare_json = map_to_figshare_json(data)
+    figshare_json = map_to_figshare_json(data,True)
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer YOUR_FIGSHARE_ACCESS_TOKEN"
+        "Authorization": settings.TOKEN
     }
-    response = requests.put(f'https://api.figshare.com/v2/private_articles/{article_id}', json=figshare_json, headers=headers)
+    response = requests.put(f'https://api.figshare.com/v2/account/articles/{article_id}', json=figshare_json, headers=headers)
     if response.status_code == 200:
         print(f"Article '{data['title']}' updated successfully on Figshare.")
     else:
@@ -118,7 +130,6 @@ def update_figshare_article(article_id, data):
 # Function to handle the entire process
 def process_data_and_push_to_figshare(record, directory_path):
     # Create or update articles on Figshare
-
     existing_article_id = check_if_article_exists(record['title'])
     if existing_article_id:
         update_figshare_article(existing_article_id, record)
@@ -134,6 +145,7 @@ def process_data_and_push_to_figshare(record, directory_path):
 def push_to_figshare(request):
     # Specify the directory containing the source files
     root_directory = settings.STAKEHOLDERS_ROOT
+
     
     for root, dirs, files in os.walk(root_directory):
         for file_name in files:
@@ -143,6 +155,9 @@ def push_to_figshare(request):
             try:
                 # Process the data and push it to Figshare
                 process_data_and_push_to_figshare(file_content, source_file)
-                return JsonResponse({'status': 'success', 'message': 'Data pushed to Figshare successfully'})
             except Exception as e:
-                return JsonResponse({'status': 'error', 'message': str(e)})
+                print("error occure while processing", file_name, e)
+                continue
+
+    return Response({'status': 'success', 'message': 'Data pushed to Figshare successfully'})
+
